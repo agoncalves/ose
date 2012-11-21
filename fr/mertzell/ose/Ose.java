b6b796a37c66095810b999dd10ad4ab234a23302
agoncalves <anthony.jose.goncalves@gmail.com>
2012-11-21 17:30:49 +0100

    #1

--- fr/mertzell/ose/Ose.java
+++ fr/mertzell/ose/Ose.java
@@ -7,6 +7,6 @@ public class Ose {
 	public static void main(String [] args){
 		PlanSalle modele = new PlanSalle("Sans Nom") ;
 		FenetrePrincipale vue = new FenetrePrincipale(modele) ;
-		new Controleur(vue,modele) ;
+		new Controleur(vue,modele) ; 
 	}
 }
